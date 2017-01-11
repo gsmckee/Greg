4e5331a0a7e8b48df7c683e1f41967d8ad17855d
gsmckee <gregory.stuart.mckee@gmail.com>
2017-01-11 13:53:22 -0800

    Methods
    Set and Get methods

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -11,15 +11,18 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Person p = new Person();
-            p.m_age = 34;
-            p.m_FName = "Mad Max";
-            p.m_LName = "A Million";
-            Person p1;
-            p1.m_age = 18;
+            //p.m_age = 34;
+            //p.m_FName = "Mad Max";
+            //p.m_LName = "A Million";
+            Person p2= new Person();
+            //p1.m_age = 18;
             // the line above does not work because "p1" does not match any existing object. Due to no instantiation of the object "p1" is trying to reference.
             // "p1" is referred to as a variable because a reference is a type of varable. 
             // Note: int, float, double, decimal, char and string are boths objects and references as the same time. 
             // There is no need to assign an object of an "int" or other integral variable type reference.
+
+            // the following line is possible because SetFName is a public method
+            p.SetFName(" Max");
         }
     }
 }
